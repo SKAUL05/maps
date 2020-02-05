b1596dfdd3212a67f076c0f06368e18430285689
Sarath Kaul <sarath.kaul@searce.com>
2020-02-05 15:28:33 +0530

    Overlay Added

--- map.py
+++ map.py
@@ -1,52 +1,84 @@
 # Created by sarathkaul on 04/02/20
+# pylint: disable=import-error
 
-# Enter file description here
+"""
+Data Driven Maps with Folium and Leaflet.js
+"""
 
-import folium
 import os
 import json
+import folium
 
 # Create map object
-map = folium.Map(location=[42.3601, -71.0589],zoom_start=12)
+_MAP_OBJECT = folium.Map(location=[32.95, 74.87], zoom_start=12)
 
 # Global tooltip
-tooltip = "Click For More Info"
+_TOOLTIP = "Click For More Info"
 
 # Create custom marker icon
-logo_icon = folium.features.CustomIcon('logo.jpg',icon_size=(30,30))
+_LOGO_ICON = folium.features.CustomIcon("logo.jpg", icon_size=(30, 30))
 
 # Vega data
-vis = os.path.join('data','vis.json')
+_VIS = os.path.join("data", "vis.json")
+
+# Geojson data
+_OVERLAY = os.path.join("data", "overlay.json")
 
 # Create markers
-folium.Marker([42.363600, -71.099500], popup='<strong> Location One </strong>',
-              tooltip=tooltip).add_to(map)
+folium.Marker(
+    [32.95277692166277, 74.87466583251953],
+    popup="<strong> Location One </strong>",
+    tooltip=_TOOLTIP,
+).add_to(_MAP_OBJECT)
 
-folium.Marker([42.333600, -71.199500], popup='<strong> Location Two </strong>',
-              tooltip=tooltip, icon = folium.Icon(icon='cloud')).add_to(map)
+folium.Marker(
+    [32.85277692166277, 74.87466583251953],
+    popup="<strong> Location Two </strong>",
+    tooltip=_TOOLTIP,
+    icon=folium.Icon(icon="cloud"),
+).add_to(_MAP_OBJECT)
 
-folium.Marker([42.377120, -71.097500], popup='<strong> Location Three </strong>',
-              tooltip=tooltip, icon = folium.Icon(color='purple')).add_to(map)
+folium.Marker(
+    [32.75289692166277, 74.77486583251953],
+    popup="<strong> Location Three </strong>",
+    tooltip=_TOOLTIP,
+    icon=folium.Icon(color="purple"),
+).add_to(_MAP_OBJECT)
 
-folium.Marker([42.263600, -71.099500], popup='<strong> Location Four </strong>',
-              tooltip=tooltip, icon = folium.Icon(color='green',icon = 'leaf')).add_to(map)
+folium.Marker(
+    [32.7345692166277, 74.76462583251953],
+    popup="<strong> Location Four </strong>",
+    tooltip=_TOOLTIP,
+    icon=folium.Icon(color="green", icon="leaf"),
+).add_to(_MAP_OBJECT)
 
-folium.Marker([42.273600, -71.199500], popup='<strong> Location Five </strong>',
-              tooltip=tooltip, icon = logo_icon).add_to(map)
+folium.Marker(
+    [32.752222692166277, 74.77422283251953],
+    popup="<strong> Location Five </strong>",
+    tooltip=_TOOLTIP,
+    icon=_LOGO_ICON,
+).add_to(_MAP_OBJECT)
 
-folium.Marker([42.334600, -71.099500], popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(vis)),width=450, height=250))).add_to(map)
-
+folium.Marker(
+    [32.75677672166277, 74.77466583241953],
+    popup=folium.Popup(max_width=450).add_child(
+        folium.Vega(json.load(open(_VIS)), width=450, height=250)
+    ),
+).add_to(_MAP_OBJECT)
 
 
 # Circle marker
 folium.CircleMarker(
-    location=(42.46670, -70.34567),
-    radius = 50,
-    popup = "My Birthplace",
-    color = "#428bca",
-    fill = True,
-    fill_color = "#428bca"
-).add_to(map)
+    location=(32.75277692166277, 74.77466583251953),
+    radius=50,
+    popup="My Birthplace",
+    color="#428bca",
+    fill=True,
+    fill_color="#428bca",
+).add_to(_MAP_OBJECT)
+
+# Geojson overlay
+folium.GeoJson(_OVERLAY, name="Jammu").add_to(_MAP_OBJECT)
 
 # Generate map
-map.save('map.html')
+_MAP_OBJECT.save("map.html")
